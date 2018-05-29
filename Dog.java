2a9a98ef4d12e82f89e8698dd86ee4368de19ca6
shruthi00 <svenkata@stuy.edu>
2018-05-29 14:03:28 -0400

    added Dog class

---
+++ Dog.java
@@ -0,0 +1,55 @@
+public abstract class Dog {
+
+public static final MAXSCARED;
+
+//instance vars
+private int cuteness;
+private int agility;
+private int loud; //loudness
+private int power;
+public int scared;
+public boolean isDefeated;
+
+//modifier methods
+public int getCuteness() {
+	return cuteness;
+}
+
+public int getAgility() {
+	return agility;
+}
+
+public int getLoud() {
+	return loud;
+}
+
+public int getPwr() {
+	return power;
+}
+
+public int getScared() {
+	return scared;
+}
+
+//modifier methods
+public void setCuteness(int x) {
+	
+}
+
+public void setAgility(int x) {
+	
+}
+
+public void setLoud(int x) {
+	
+}
+
+public void setPwr(int x) {
+	
+}
+
+public void setScared(int x) {
+	
+}
+
+}
