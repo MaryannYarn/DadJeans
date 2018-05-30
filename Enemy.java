d70066987b5e23db593bf673759e850c8cfea983
shruthi00 <svenkata@stuy.edu>
2018-05-30 14:00:31 -0400

    added Enemy abstract class

---
+++ Enemy.java
@@ -0,0 +1,27 @@
+public abstract class Enemy {
+
+//instance vars
+private static final BASEHP;
+private static final BASECUTE;
+private int HP;
+private int withstoodCuteness;
+
+//methods
+public boolean isDefeated() {
+  return false;
+}
+
+public void setHP(int x) {
+}
+
+public void setCute(int x) {
+
+}
+
+public void getHP(int x) {
+
+}
+
+
+
+}
